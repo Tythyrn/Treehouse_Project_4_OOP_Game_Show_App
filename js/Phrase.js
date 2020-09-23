31ff340e592add0d1306a8b3a225aa89e9e3bec2
GentlemanMenace <kcrundquist@gmail.com>
2020-09-22 20:44:45 -0500

    Set up project and first pass at Phrase class

---
+++ js/Phrase.js
@@ -0,0 +1,52 @@
+/* Treehouse FSJS Techdegree
+ * Project 4 - OOP Game App
+ * Phrase.js */
+class Phrase {
+    constructor (phrase) {
+        this.phrase = phrase.toLowerCase();;
+    }
+
+    /**
+     * addPhraseToDisplay takes the phrase of the class and adds it to the screen
+     */
+    addPhraseToDisplay() {
+        const phraseUl = document.getElementById('phrase').firstChild;
+        for (let character of phrase) {
+            let li = document.createElement('li');
+
+            if (character !== " "){
+                li.classList.add(`hide letter ${character}`);
+            } else {
+                li.classList.add('space');
+            }
+
+            li.textContent = character;
+            phraseUl.appendChild(li);
+        }
+    }
+
+    /**
+     * checkLetter validates if a player's guess is correct
+     * @param {string} guess guess of the player
+     * @returns {boolean} returns if the guess was correct or not
+     */
+    checkLetter (guess) {
+        for (let character of phrase) {
+            if (character === guess) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * showMatchedLetter reveals all the correct letters based on the player's guess
+     * @param {string} guess guess of the player
+     */
+    showMatchedLetter(guess) {
+        const matchedLi = document.getElementsByClassName(guess);
+        for (let li of matchedLi) {
+            li.classList.replace('hide', 'show');
+        }
+    } 
+}
